a2bfd51036295fb509e8cd7a765fbad14a5ef53a
johnnyzuniga <john_isaac_zuniga@dlsu.edu.ph>
2024-10-19 14:56:22 +0800

    index page

--- app/src/main/java/mobdeve/com/s20/group3/mco/MainActivity.java
+++ app/src/main/java/mobdeve/com/s20/group3/mco/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
